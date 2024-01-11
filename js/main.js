97d3becc6e38bfe9ccf62e2f14e39a4fb0aa52e1
Ar Rasyid Sarifullah <rasyidzkun@gmail.com>
2024-01-11 08:42:08 +0700

    feat: scroll to top arrow

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ const menuBar = document.querySelector(".menu-bar");
 const menuList = document.querySelector(".nav-menu");
 const listItems = document.querySelectorAll(".nav-link");
 
+const scrollTop = document.getElementById("scroll-top");
+
+window.addEventListener("scroll", () => {
+  window.scrollY > 100
+    ? scrollTop.classList.remove("none")
+    : scrollTop.classList.add("none");
+});
+
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("is-active");
   menuBar.classList.toggle("is-active");
